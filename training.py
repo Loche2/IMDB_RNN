f8544284ae4ee07e6ebbbc5a146505715d5a251a
LocheZ <1913565532@qq.com>
2023-10-03 00:11:15 +0800

    Completed Version

--- training.py
+++ training.py
@@ -1,4 +1,5 @@
 import random
+import spacy
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -50,12 +51,30 @@ class RNN(nn.Module):
         return predictions
 
 
+class LSTM(nn.Module):
+    def __init__(self, inputs, embedding, hidden, outputs):
+        super().__init__()
+        self.embedding = nn.Embedding(inputs, embedding)
+        self.lstm = nn.LSTM(embedding, hidden)
+        self.fc1 = nn.Linear(hidden, hidden // 2)
+        self.fc2 = nn.Linear(hidden // 2, outputs)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, text):
+        embedded = self.embedding(text)
+        output, (hidden, cell) = self.lstm(embedded)
+        predictions = self.relu(self.fc1(hidden[-1]))  # Use the last hidden state
+        predictions = self.fc2(predictions)
+        return predictions
+
+
 # 初始化模型和优化器
 INPUT = len(TEXT.vocab)
 EMBEDDING = 100
-HIDDEN = 50
+HIDDEN = 128
 OUTPUT = 1
-model = RNN(INPUT, EMBEDDING, HIDDEN, OUTPUT)
+# model = RNN(INPUT, EMBEDDING, HIDDEN, OUTPUT)
+model = LSTM(INPUT, EMBEDDING, HIDDEN, OUTPUT)
 optimizer = optim.Adam(model.parameters())
 criterion = nn.BCEWithLogitsLoss()
 # 将模型和损失函数移至设备
@@ -76,7 +95,7 @@ def train(model, iterator, optimizer, criterion):
     model.train()
     epoch_loss = 0
     epoch_accuracy = 0
-    for batch in tqdm(iterator, desc=f'Epoch [{epoch + 1}/{EPOCHS}]'):
+    for batch in tqdm(iterator, desc=f'Epoch [{epoch + 1}/{EPOCHS}]', delay=0.1):
         optimizer.zero_grad()
         predictions = model(batch.text[0]).squeeze(1)
         loss = criterion(predictions, batch.label)
@@ -108,8 +127,29 @@ EPOCHS = 10
 for epoch in range(EPOCHS):
     train_loss, train_accuracy = train(model, train_iterator, optimizer, criterion)
     valid_loss, valid_accuracy = evaluate(model, valid_iterator, criterion)
-    print(f'Epoch: {epoch + 1:02}')
     print(f'\tTrain Loss: {train_loss:.3f} | Train Accuracy: {train_accuracy * 100:.2f}%')
     print(f'\tValidation Loss: {valid_loss:.3f} | Validation Accuracy: {valid_accuracy * 100:.2f}%')
 
+# 测试模型
+test_loss, test_accuracy = evaluate(model, test_iterator, criterion)
+print(f'\nTest Loss: {test_loss:.3f} | Test Acc: {test_accuracy * 100:.2f}%')
+nlp = spacy.load('en_core_web_sm')
 
+
+# 使用模型进行预测
+def predict_sentiment(model, sentence, threshold=0.5):
+    model.eval()
+    tokenized = [tok.text for tok in nlp.tokenizer(sentence)]
+    indexed = [TEXT.vocab.stoi[t] for t in tokenized]
+    tensor = torch.LongTensor(indexed).to(device)
+    tensor = tensor.unsqueeze(1)
+    prediction = torch.sigmoid(model(tensor))
+    print(f'Proba: {prediction.item()}')
+    return 'positive' if prediction.item() > threshold else 'negative'
+
+
+# 示例预测
+positive_comment = "What a fantastic film!"
+negative_comment = "I was disappointed by this movie."
+print(f'Comment: {positive_comment}\nPrediction: {predict_sentiment(model, positive_comment)}')
+print(f'Comment: {negative_comment}\nPrediction: {predict_sentiment(model, negative_comment)}')
